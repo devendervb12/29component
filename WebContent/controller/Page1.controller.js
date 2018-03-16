4011a6e20d3cc4b39215b56b15c9dfac35e4501c
devendervb12 <you@example.com>
2018-03-16 09:24:42 -0700

    Passing data via router

--- WebContent/controller/Page1.controller.js
+++ WebContent/controller/Page1.controller.js
@@ -1,7 +1,9 @@
 sap.ui.controller("smax.batch29.A2.controller.Page1", {
 
-	onItemSelection : function(){
-		this.getOwnerComponent().getRouter().navTo("rPage2");
+	onItemSelection : function(oEvent){
+		debugger;
+		// get selected pid
+		this.getOwnerComponent().getRouter().navTo("rPage2", {pid : oEvent.getSource().getTitle()});
 	}
 
 });
--- WebContent/controller/Page2.controller.js
+++ WebContent/controller/Page2.controller.js
@@ -1,5 +1,21 @@
 sap.ui.controller("smax.batch29.A2.controller.Page2", {
 
+	onInit : function(){
+	    var oRouter = this.getOwnerComponent().getRouter();
+	    
+	    oRouter.getRoute("rPage2").attachPatternMatched(function(oEvent){
+	    	debugger;
+	    	var pid = oEvent.getParameters().arguments.pid;
+	    	
+	    	//now call /ProductSet('+pid+') 
+	    	this.getView().bindElement("/ProductSet('"+pid+"')");
+	    	
+	    }, this);
+	    
+	    
+	    
+	},
+	
 	goBack : function(){
 		this.getOwnerComponent().getRouter().navTo("rPage1");
 	}
